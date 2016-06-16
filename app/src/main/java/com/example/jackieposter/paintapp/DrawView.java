fae73379844ac4f3fd7b08b51cfa23551a7e19ab
Jackie Poster <jackieposter@Jackies-MacBook-Pro.local>
2016-06-16 16:51:32 -0400

    different pen widths and colors, eraser, new canvas and save image

--- app/src/main/java/com/example/jackieposter/paintapp/DrawView.java
+++ app/src/main/java/com/example/jackieposter/paintapp/DrawView.java
@@ -1,10 +1,26 @@
 package com.example.jackieposter.paintapp;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.view.View;
 import android.util.AttributeSet;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
 
 public class DrawView extends View {
+
+    private Path drawPath;
+    private Paint drawPaint;
+    private Paint canvasPaint;
+    private int paintColor = Color.rgb(0,0,0);
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+    private boolean erase = false;
 
     public DrawView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -12,5 +28,80 @@ public class DrawView extends View {
     }
 
     private void setUpDrawing() {
+
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+
+        invalidate();
+        return true;
+    }
+
+    public void setPenWidth(int size){
+        invalidate();
+        drawPaint.setStrokeWidth(size);
+    }
+
+    public void setPenColor(int color){
+        invalidate();
+        drawPaint.setColor(color);
+    }
+
+    public void setErase(boolean isErase){
+        erase = isErase;
+        if(erase){
+            drawPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        }
+        else {
+            drawPaint.setXfermode(null);
+        }
+    }
+
+    public void startNew(){
+        drawCanvas.drawColor(0,PorterDuff.Mode.CLEAR);
+        invalidate();
     }
 }
--- app/src/main/java/com/example/jackieposter/paintapp/MainActivity.java
+++ app/src/main/java/com/example/jackieposter/paintapp/MainActivity.java
@@ -1,13 +1,161 @@
 package com.example.jackieposter.paintapp;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawing)
+    DrawView drawView;
+
+    private int red = Color.rgb(255,0,0);
+    private int orange = Color.rgb(255,127,0);
+    private int yellow = Color.rgb(255,255,0);
+    private int green = Color.rgb(116,219,33);
+    private int blue = Color.rgb(0,0,255);
+    private int purple = Color.rgb(127,0,127);
+    private int black = Color.rgb(0,0,0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.penSmall)
+    public void penSmallClicked() {
+        int smallBrush = getResources().getInteger(R.integer.small_size);
+        drawView.setErase(false);
+        drawView.setPenWidth(smallBrush);
+    }
+
+    @OnClick(R.id.penMedium)
+    public void penMediumClicked() {
+        int mediumBrush = getResources().getInteger(R.integer.medium_size);
+        drawView.setErase(false);
+        drawView.setPenWidth(mediumBrush);
+    }
+
+    @OnClick(R.id.penLarge)
+    public void penLargeClicked() {
+        int largeBrush = getResources().getInteger(R.integer.large_size);
+        drawView.setErase(false);
+        drawView.setPenWidth(largeBrush);
+    }
+
+    @OnClick(R.id.redFab)
+    public void onRedClicked(){
+        drawView.setErase(false);
+        drawView.setPenColor(red);
+    }
+
+    @OnClick(R.id.orangeFab)
+    public void onOrangeClicked(){
+        drawView.setErase(false);
+        drawView.setPenColor(orange);
+    }
+
+    @OnClick(R.id.yellowFab)
+    public void onYellowClicked(){
+        drawView.setErase(false);
+        drawView.setPenColor(yellow);
+    }
+
+    @OnClick(R.id.greenFab)
+    public void onGreenClicked(){
+        drawView.setErase(false);
+        drawView.setPenColor(green);
+    }
+
+    @OnClick(R.id.blueFab)
+    public void onBlueClicked() {
+        drawView.setErase(false);
+        drawView.setPenColor(blue);
+    }
+
+    @OnClick(R.id.purpleFab)
+    public void onPurpleClicked(){
+        drawView.setErase(false);
+        drawView.setPenColor(purple);
+    }
+
+    @OnClick(R.id.blackFab)
+    public void onBlackClicked(){
+        drawView.setErase(false);
+        drawView.setPenColor(black);
+    }
+
+    @OnClick(R.id.erase)
+    public void onEraseClicked(){
+        drawView.setErase(true);
+    }
+
+    @OnClick(R.id.newDrawing)
+    public void onNewDrawingClicked(){
+        AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
+        newDialog.setTitle("New drawing");
+        newDialog.setMessage("Start new drawing (you will lose the current drawing)?");
+        newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                drawView.startNew();
+                dialog.dismiss();
+            }
+        });
+        newDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                dialog.cancel();
+            }
+        });
+        newDialog.show();
+    }
+
+    @OnClick(R.id.save)
+    public void onSaveClicked() {
+        AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+        saveDialog.setTitle("Save drawing");
+        saveDialog.setMessage("Save drawing to device Gallery?");
+        saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                //save drawing
+                drawView.setDrawingCacheEnabled(true);
+
+                String imgSaved = MediaStore.Images.Media.insertImage(
+                        getContentResolver(), drawView.getDrawingCache(),
+                        UUID.randomUUID().toString()+".png", "drawing");
+
+                if(imgSaved!=null){
+                    Toast savedToast = Toast.makeText(getApplicationContext(),
+                            "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                    savedToast.show();
+                }
+                else{
+                    Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                            "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                    unsavedToast.show();
+                }
+
+                drawView.destroyDrawingCache();
+            }
+
+        });
+        saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                dialog.cancel();
+            }
+        });
+        saveDialog.show();
     }
 }
