b76bd83a446c4c21f9650bfd4452fb709c278f19
Jackie Poster <jackieposter@Jackies-MacBook-Pro.local>
2016-06-16 10:00:54 -0400

    Initial commit with UI elements added

---
+++ app/src/main/java/com/example/jackieposter/paintapp/DrawView.java
@@ -0,0 +1,16 @@
+package com.example.jackieposter.paintapp;
+
+import android.content.Context;
+import android.view.View;
+import android.util.AttributeSet;
+
+public class DrawView extends View {
+
+    public DrawView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setUpDrawing();
+    }
+
+    private void setUpDrawing() {
+    }
+}
